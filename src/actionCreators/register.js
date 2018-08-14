5e142b09830e99a67824dc73e68b6dfda8112a91
tonymputhia <kimathitony93@gmail.com>
2018-08-14 10:06:40 +0300

    [Bug #159756566] Modify authentication feature to resolve bugs

--- src/actionCreators/register.js
+++ src/actionCreators/register.js
@@ -1,39 +1,35 @@
 import axios from 'axios';
-import {createBrowserHistory} from 'history';
+import history from '../history'
 import swal from 'sweetalert';
 import { api_url } from './../config';
 
-export const register = (dispatch, data) => {
-  const browserHistory = createBrowserHistory();
-  const register_url = `${api_url}register`;
-  axios
-    .post(register_url, {
-      username: data.username,
-      email: data.email,
-      password: data.password,
-      confirm_password: data.confirmPassword
-    })
-    .then(res => {
-      const Message = res.data.Message;
-      dispatch({ type: 'REGISTER_SUCCESS', data: { Message } });
-      swal(res.data.message);
-      browserHistory.push('/login');
-    })
-    .catch(error => {
-        console.log(error)
-      if (error.response.status === 406) {
-        const Message = error.response.data.message;
-        dispatch({ type: 'REGISTER_FAIL', data: { Message } });
-        swal('NOT ACCEPTABLE', Message, 'error');
-      } else if (error.response.status === 409) {
-        const Message = error.response.data.message;
-        dispatch({ type: 'REGISTER_FAIL', data: { Message } });
-        swal('ERROR!', Message, 'error');
-      }
-      else if (error.response.status === 401) {
-        const Message = error.response.data.message;
-        dispatch({ type: 'REGISTER_FAIL', data: { Message } });
-        swal('ERROR!', Message, 'error');
-      }
-    });
+/**
+ * Register user function
+ * @param{Object} - data
+ * @returns{String} - Message
+ */
+export const register = data => {
+  return dispatch => {
+    const register_url = `${api_url}register`;
+    dispatch({'type':'AUTH_FETCHING'});
+    return axios
+      .post(register_url, {
+        username: data.username,
+        email: data.email,
+        password: data.password,
+        confirm_password: data.confirm_password
+      })
+      .then(res => {
+        const Message = res.data.message;
+        dispatch({ type: 'REGISTER_SUCCESS', data: { Message } });
+        swal(Message);
+        history.push('/login');
+      })
+      .catch(error => {
+        if (error.response.status === 401) {
+          const Message = error.response.data.message;
+          dispatch({ type: 'REGISTER_FAIL', data: { Message } });
+        }
+      });
+  };
 };

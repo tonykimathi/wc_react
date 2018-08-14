db6a878b400733e6d0e8a60a13e0dcc3c9fee92e
tonymputhia <kimathitony93@gmail.com>
2018-08-14 10:01:58 +0300

    [Bug #159756566] Modify authentication feature to resolve bug

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,73 +1,130 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
-import { login } from '../actionCreators/login';
-import '../styles/index.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import history from '../history';
+import { Button, Form, Label, Input, Alert } from 'reactstrap';
+import { login } from '../actionCreators/Login';
+import { loader } from './../config';
 import PropTypes from 'prop-types';
 
-class Login extends React.Component {
+/**
+ * This component render login page
+ */
+class Login extends Component {
   constructor() {
-      super();
-      this.state = {}
+    super();
+    this.state = {
+      visible: true
+    };
   }
+  /**
+   * redirect a user to businesses page after logging in
+   */
+  componentDidMount() {
+    if (this.props.auth.loggedIn) {
+      history.push('/businesses');
+    }
+  }
+
   _onDismiss = () => {
     this.setState({
       visible: false
     });
   };
 
-  _login = (e) => {
-      e.preventDefault();
-      const email = e.target.elements.email.value;
-      const password = e.target.elements.password.value;
-      this.props.login({ email, password });
-      this.forceUpdate();
+  /**
+   * Makes a server request to log in a user
+   */
+  _login = e => {
+    e.preventDefault();
+    const email = e.target.elements.email.value;
+    const password = e.target.elements.password.value;
+    this.props.login({ email, password });
+    this.forceUpdate();
   };
-    render() {
+
+  render() {
     return (
-      <Container>
-        <Row>
-          <Col xs="6" sm="4"></Col>
-          <Col xs="6" sm="4">
-           <div className="container">
-           <h1>Login</h1>
-            <Form onSubmit={this._login}>
-                <FormGroup>
-                <Label for="Email">Email</Label>
-                <Input type="email" name="email" id="Email" placeholder="Enter Email Address" />
-                </FormGroup>
-                <FormGroup>
-                <Label for="Password">Password</Label>
-                <Input type="password" name="password" id="Password" placeholder="Enter Password" />
-                </FormGroup>
-                <Button>Submit</Button>
-            </Form>
+      <div className="loginform">
+        <h1>Login</h1>
+        <p>Enter you credentials to login.</p>
+        <hr />
+        {this.props.auth.error ? (
+          <Alert
+            isOpen={this.state.visible}
+            color="danger"
+            toggle={this._onDismiss}
+          >
+            {this.props.auth.Message}
+          </Alert>
+        ) : null}
+        <Form onSubmit={this._login}>
+          <div className="form-group">
+            <Label for="exampleEmail">
+              <b>Email</b>
+            </Label>
+            <Input
+              type="email"
+              name="email"
+              placeholder="name@email.com"
+              required
+              autoComplete="email"
+            />
           </div>
-          </Col>
-          <Col sm="4"></Col>
-        </Row>
-      </Container>    
+          <div className="form-group">
+            <Label for="examplePassword">
+              <b>Password</b>
+            </Label>
+            <Input type="password" name="password" placeholder="password" />
+          </div>
+          <div className="form-group" id="submit">
+            {!this.props.auth.loading ? (
+              <Button type="submit">
+                Login
+              </Button>
+            ) : (
+              loader
+            )}
+          </div>
+          <br />
+          <br />
+          <div>
+            <span className="psw">
+              <Link to="/requestreset">Forgot password?</Link>
+            </span>
+          </div>
+        </Form>
+      </div>
     );
-    }
-  
+  }
+
   _handleSubmit(event) {
     event.preventDefault();
+  }
+}
+
+/**
+ * Map store state to props
+ */
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  };
 };
-}
 
-const mapStateToProps = (state) => {
-return {
-    auth : state.auth
-}
-}
+const mapDispatchToProps = dispatch => ({
+  login: data => dispatch(login(data))
+});
 
-const mapDispatchToProps = (dispatch) => ({
-login: (data) => login(dispatch,data)
-})
-
+/**
+ * Validate props
+ */
 Login.propTypes = {
   auth: PropTypes.object,
   login: PropTypes.func
 };
 
-export default connect(mapStateToProps,mapDispatchToProps)(Login);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,70 +1,151 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
-import { register } from '../actionCreators/register';
-import '../styles/index.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Button, Form, Label, Input, Alert } from 'reactstrap';
+import { register } from '../actionCreators/Register';
+import { loader } from './../config';
 
-class Register extends React.Component {
+/**
+ * This component render sign up page
+ */
+class Register extends Component {
   constructor() {
-      super();
-      this.state = {}
+    super();
+    this.state = {
+      visible: true
+    };
   }
 
-  _register = (e) => {
-      e.preventDefault();
-      const username = e.target.elements.username.value;
-      const email = e.target.elements.email.value;
-      const password = e.target.elements.password.value;
-      const confirmPassword = e.target.elements.confirmPassword.value;
-      this.props.register({email, username, password, confirmPassword})
+  /**
+   * The function closes the alert box
+   */
+  _onDismiss = () => {
+    this.setState({
+      visible: false
+    });
   };
-    render() {
+
+  /**
+   * Makes a server request to register a new user
+   * @param {Odject} - data
+   * @return {string} message
+   */
+  _register = e => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const email = e.target.elements.email.value;
+    const password = e.target.elements.password.value;
+    const confirm_password = e.target.elements.confirm_password.value;
+    this.props.register({ email, username, password, confirm_password });
+  };
+  render() {
     return (
-      <Container>
-        <Row>
-          <Col xs="6" sm="4"></Col>
-          <Col xs="6" sm="4">
-           <div className="container">
-            <Form onSubmit={this._register}>
-                <FormGroup>
-                <Label for="Email">Username</Label>
-                <Input type="text" name="username" id="Username" placeholder="Enter Username" />
-                </FormGroup>  
-                <FormGroup>
-                <Label for="Email">Email</Label>
-                <Input type="email" name="email" id="Email" placeholder="Enter Email Address" />
-                </FormGroup>
-                <FormGroup>
-                <Label for="Password">Password</Label>
-                <Input type="password" name="password" id="Password" placeholder="Enter Password" />
-                </FormGroup>
-                <FormGroup>
-                <Label for="CPassword">Confirm Password</Label>
-                <Input type="password" name="cpassword" id="CPassword" placeholder="Confirm Password" />
-                </FormGroup>
-                <Button>Submit</Button>
-            </Form>
+      <div className="regform">
+        <h1>Sign Up</h1>
+        <p>Fill in this form to create an account.</p>
+        <hr />
+        {this.props.auth.error ? (
+          <Alert
+            isOpen={this.state.visible}
+            color="danger"
+            toggle={this._onDismiss}
+          >
+            {this.props.auth.Message}
+          </Alert>
+        ) : null}
+        <Form onSubmit={this._register}>
+
+          <div className="form-group">
+            <Label for="username">
+              <b>Username</b>
+            </Label>
+            <Input
+              type="text"
+              name="username"
+              placeholder="username"
+              required
+            />
           </div>
-          </Col>
-          <Col sm="4"></Col>
-        </Row>
-      </Container>    
+
+          <div className="form-group">
+            <Label for="email">
+              <b>Email</b>
+            </Label>
+            <Input
+              type="email"
+              name="email"
+              placeholder="name@email.com"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <Label for="Password">
+              <b>Password</b>
+            </Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="password"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <Label for="confirmPassword">
+              <b>Confirm Password</b>
+            </Label>
+            <Input
+              type="password"
+              name="confirm_password"
+              placeholder="confirm password"
+              required
+            />
+          </div>
+          <div className="form-group" id="submit">
+            {!this.props.auth.loading ? (
+              <Button type="submit">
+              Sign up
+              </Button>
+            ) : (
+              loader
+            )}
+          </div>
+
+          <br />
+          <br />
+        </Form>
+      </div>
     );
-    }
-  
+  }
+
   _handleSubmit(event) {
     event.preventDefault();
+  }
+}
+
+/**
+ * Map store state to props
+ */
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  };
 };
-}
 
-const mapStateToProps = (state) => {
-return {
-    auth : state.auth
-}
-}
+const mapDispatchToProps = dispatch => ({
+  register: data => dispatch(register(data))
+});
 
-const mapDispatchToProps = (dispatch) => ({
-register: (data) => register(dispatch,data)
-})
+/**
+ * Validate props
+ */
+Register.propTypes = {
+  auth: PropTypes.object,
+  register: PropTypes.func
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(Register);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Register);
